34168e9f8218140a0707b02bcc23224be0f97e09
Kiefer Levine <kiefer_8@hotmail.com>
2021-07-20 15:51:58 -0400

    Implemented Sequelize and Handlebars
    I forgot to push the database to the repo before starting on the Handlebars. So sue me.

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -6,6 +10,16 @@ const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handlebars setup
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Turn on routes
+app.use(routes);
 
 // Turn on connection to server
-app.listen(PORT, () => console.log('Now listening'));
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
